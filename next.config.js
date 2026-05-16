599723d8ce1c9b95fd1dd31906949537121083ee
NysaPahari <nysa.pahari@gmail.com>
2026-05-16 11:03:59 +0530

    Update mock OA feature

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
+  turbopack: {
+    root: __dirname,
+  },
   rewrites: async () => {
     return [
       {
